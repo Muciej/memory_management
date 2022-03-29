56f4f0adf3f7bb3ed06b80685f94d231114f38f2
Muciej <261745@student.pwr.edu.pl>
2022-03-29 17:13:42 +0200

    zadanie 6

--- example6.cpp
+++ example6.cpp
@@ -4,10 +4,12 @@
 
 using ShoppingList = std::shared_ptr<std::FILE>;
 
+auto deleterList = [](FILE* f){ fclose(f);};
+
 ShoppingList makeFile(const char* filename, const char* flags)
 {
-    /*Edit here*/
-    return std::shared_ptr<std::FILE>();
+    auto file = ShoppingList( fopen(filename, flags), deleterList );
+    return file;
 }
 
 class Partner
@@ -15,9 +17,10 @@ class Partner
 public:
     void addToFile(std::string element)
     {
-        /*Edit here*/
+        fprintf(shoppingList.get(), "%s\n", element.c_str() );
     }
     ShoppingList shoppingList;
+    
 };
 
 int main()
